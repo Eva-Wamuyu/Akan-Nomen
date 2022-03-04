f62b3e12a448472f25e2a5c0b129d05e2fcecb4d
Eva <rutheve.eva@gmail.com>
2022-03-04 14:14:59 +0300

    ft: add gender validation function

--- resources/script.js
+++ resources/script.js
@@ -5,3 +5,31 @@ const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
 
 let genderFemale = document.getElementById("female");
 let genderMale = document.getElementById("male");
+
+let genderWarning = document.getElementById("genderWarning");
+
+
+const validateGender = ()=>{
+
+  if(genderFemale.checked == false && genderMale.checked == false)
+  {
+    return "Please select your gender";
+  }
+
+  return "";
+
+}
+
+button.addEventListener("click",()=>{
+
+
+  genderWarning.textContent = validateGender();
+
+  
+
+  
+
+
+
+
+});
