c0d55c8c750d94c7ac5cd0f9c70e0800f0676e66
Eva <rutheve.eva@gmail.com>
2022-03-04 14:02:36 +0300

    ft: initiate variables

---
+++ resources/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("submit");
+
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+let genderFemale = document.getElementById("female");
+let genderMale = document.getElementById("male");
